aec2f8edb6365f3788286e5d3d11f461c46fdaab
anakon <nazarkinah73@gmail.com>
2024-09-18 00:14:04 +0300

    chore: initial project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import antfu from '@antfu/eslint-config'
+import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind'
+
+export default antfu({
+  plugins: {
+    'readable-tailwind': eslintPluginReadableTailwind,
+  },
+  rules: {
+    'import/order': [
+      'warn',
+      { groups: ['builtin', 'external'], warnOnUnassignedImports: true },
+    ],
+    'unused-imports/no-unused-imports': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+    ...eslintPluginReadableTailwind.configs.warning.rules,
+    ...eslintPluginReadableTailwind.configs.error.rules,
+    'readable-tailwind/multiline': ['warn', { printWidth: 90 }],
+  },
+})
