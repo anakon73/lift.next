8e44e5df84ca6210832c585486c9bac775048fcc
anakon <nazarkinah73@gmail.com>
2024-09-27 00:39:49 +0300

    build(vite): unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,6 +9,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: ['vue'],
+      dts: true,
+    }),
   ],
   resolve: {
     alias: {
